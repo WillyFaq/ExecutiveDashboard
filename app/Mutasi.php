a9334072eb0e60b5354e35ba004c6d05da1812d7
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-21 16:45:54 +0700

    Ubah Model `Mutasi`
    - Tambah kolom `nik`
    - Tambah relasi `bagian_lama` & `bagian_baru`
    - Karena nama kolom & relasi sama, ubah kolom: `bagian` -> `kode_bagian_baru` & `bagian_lama` -> `kode_bagian_lama`

--- app/Mutasi.php
+++ app/Mutasi.php
@@ -13,8 +13,9 @@ class Mutasi extends Model
         return parent::newQuery()
             ->addSelect([
                 'no_mutasi',
-                'bagian',
-                'bagian_lama',
+                'nik',
+                \DB::Raw('bagian AS kode_bagian_baru'),
+                \DB::Raw('bagian_lama AS kode_bagian_lama'),
                 'jabatan',
                 'keterangan',
                 'tanggal',
@@ -24,5 +25,15 @@ class Mutasi extends Model
     public function karyawan()
     {
         return $this->belongsTo(Karyawan::class, 'nik');
+    }
+
+    public function bagian_lama()
+    {
+        return $this->belongsTo(Bagian::class, 'kode_bagian_lama');
+    }
+
+    public function bagian_baru()
+    {
+        return $this->belongsTo(Bagian::class, 'kode_bagian_baru');
     }
 }
