58cd9488681439034752a482494a9f825cd08e18
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-21 16:48:35 +0700

    Ubah Model `RekapPerkuliahan`: tambah relasi `dosen`

--- app/RekapPerkuliahan.php
+++ app/RekapPerkuliahan.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class RekapPerkuliahan extends Model
 {
     protected $table = 'v_rkp';
+
+    public function dosen()
+    {
+        return $this->belongsTo(Karyawan::class, 'jkul_kary_nik');
+    }
 }
