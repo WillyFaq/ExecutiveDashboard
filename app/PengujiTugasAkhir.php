6fb9a530bf44a5b4fcf100b1ab3383bb0b9c27e8
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-21 16:51:30 +0700

    Ubah Model `PengujITugasAkhir`
    - Tambah sejumlah kolom
    - Fix join di query builder
    - Fix relasi `pembimbing1` & `pembimbing2` menyesuaikan penambahan kolom
    - Tambah relasi `mahasiswa`

--- app/PengujiTugasAkhir.php
+++ app/PengujiTugasAkhir.php
@@ -11,8 +11,16 @@ class PengujiTugasAkhir extends Model
     public function newQuery()
     {
         return parent::newQuery()
-            ->join('v_antri_proposal', 'v_propta.mhs_nim', 'v_antri_proposal.mhs_nim')
-            ->join('v_propta', 'v_propta.mhs_nim', 'v_antri_proposa.mhs_nim');
+            ->addSelect([
+                'v_propta.semester',
+                \DB::Raw('v_jdw_proposal.penguji1 nik_penguji_1'),
+                \DB::Raw('v_jdw_proposal.penguji2 nik_penguji_2'),
+                'v_propta.mhs_nim',
+                'v_propta.tgl_smn',
+                'v_propta.jdl_proposal',
+            ])
+            ->join('v_antri_proposal', 'v_jdw_proposal.kode_antrian', 'v_antri_proposal.kode_antrian')
+            ->join('v_propta', 'v_propta.mhs_nim', 'v_antri_proposal.mhs_nim');
     }
 
     public function scopeWhereStatusSelesai($query)
@@ -24,11 +32,16 @@ class PengujiTugasAkhir extends Model
 
     public function penguji1()
     {
-        return $this->belongsTo(Karyawan::class, 'v_propta.penguji1');
+        return $this->belongsTo(Karyawan::class, 'nik_penguji_1');
     }
 
     public function penguji2()
     {
-        return $this->belongsTo(Karyawan::class, 'v_propta.penguji2');
+        return $this->belongsTo(Karyawan::class, 'nik_penguji_2');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class, 'mhs_nim');
     }
 }
