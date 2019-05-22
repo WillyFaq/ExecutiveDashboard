f547bd0b0f0435164eaa66951662e9677ad66799
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-22 10:36:40 +0700

    Ubah Model `RekapEwmp`: tambahkan method untuk set `semester`
    Digunakan sebelum select karena spesifikasi tabel.

--- app/RekapEwmp.php
+++ app/RekapEwmp.php
@@ -6,7 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class RekapEwmp extends Model
 {
+    const TAHUN_AWAL = 11;
+
     protected $table = 'rekap_ewmp_lain_pc_prodi';
+
+    /**
+     * Untuk men-set semester yang digunakan ketika select tabel ini.
+     *
+     * Tabel ini agak "unik" karena jika hanya select saja akan menampilkan sebagian data.
+     * Sebelum select perlu di-set semester nya.
+     * Setelah itu hasil select akan menampilkan data pada semester tersebut.
+     *
+     * Misal untuk select data semester 182:
+     * RekapEwmp::setSemester('182');
+     * $data = RekapEwmp::get();
+     * 
+     * Misal untuk mengambil semua data:
+     * $data = collect();
+     * for($tahun = RekapEwmp::TAHUN_AWAL; $tahun <= $tahun_smt_saat_ini; $tahun++) {
+     *     for($semester = 1; $semester <= 2; $semester++) {
+     *         RekapEwmp::setSemester($tahun.$semester);
+     *         collect(RekapEwmp::get());
+     *     }
+     * }
+     *
+     * @param string $semester
+     */
+    public static function setSemester($semester)
+    {
+        $sql_query = <<<SQL
+BEGIN pkg_param.set_smt(:semester); END;
+SQL;
+        $stmt = \DB::getPdo()->prepare($sql_query);
+        $stmt->bindValue('semester', $semester);
+        $stmt->execute();
+    }
 
     public function scopeWhereIsPenelitian($query)
     {
