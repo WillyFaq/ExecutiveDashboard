d366f96e1bb88244894e975b024a0cb0743d4b8c
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-24 09:27:41 +0700

    Ubah Model `Prodi`: tambah scope `whereIsAktif`

--- app/Prodi.php
+++ app/Prodi.php
@@ -9,6 +9,11 @@ class Prodi extends Model
     protected $table = 'v_fakultas';
     protected $primaryKey = 'id';
 
+    public function scopeWhereIsAktif($query)
+    {
+        return $query->where('sts_aktif', 'Y');
+    }
+
     public function dosen()
     {
         return $this->hasMany(Karyawan::class, 'v_prodiewmp.prodi');
