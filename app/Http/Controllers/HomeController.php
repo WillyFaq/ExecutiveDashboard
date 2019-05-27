fd1f8da3d1501ecfe20be693c0b082960d277324
Rahman Nur Hadi <rahman@stikom.edu>
2019-05-27 18:24:20 +0700

    Ubah Controller `HomeController`
    - Cleanup
    - Gunakan data dari DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,128 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\NilaiBorang;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
+    public function index(Request $request, $tahun_start = 2016, $tahun_end = 2017)
+    {
+        $nilai = NilaiBorang::with('materi')
+            ->whereBetween('tahun', [$tahun_start, $tahun_end])
+            ->get()
+            ->groupBy('tahun')
+            ->map(function ($tahun) {
+                return $tahun->sum(function ($nilai) {
+                    return $nilai->nilai * ($nilai->materi->persen * 100);
+                });
+            });
+        $nilai_tahun = NilaiBorang::where('tahun', Carbon::now()->format('Y'))
+            ->whereHas('materi', function ($query) {
+                return $query
+                    ->where('kd_jns', 1)
+                    ->whereLayer(1)
+                    ->where('kd_std', '!=', '1810');
+            })
+            ->with('materi')
+            ->get()
+            ->groupBy(function ($nilai_tahun) {
+                return $nilai_tahun->materi->nm_std;
+            })
+            ->map(function ($nilai_tahun) {
+                return $nilai_tahun->first()->nilai;
+            });
 
-    public function index(){
-
-		$line = array(
-			'2011' => 320,
-			'2012' => 340,
-			'2013' => 320,
-			'2014' => 320,
-			'2015' => 320,
-			'2016' => 330,
-			'2017' => 320,
-			'2018' => 350,
-			);
-
-		$data_profil = [];
-
-    		$nilai = 2.5;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Sumber Daya Manusia", 
-    				'link' 		=> '/sdm/profil/Dosen Tetap/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'grad', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-    			)
-    		);
-    		$nilai = 3.7;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Lembaga", 
-    				'link' 		=> '/sdm/profil/Dosen Tetap/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'gradbook', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 0
-    			)
-    		);
-    		$nilai = 3.5;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Kemahasiswaan", 
-    				'link' 		=> '/sdm/profil/Lektor &#38; Guru besar/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart'		=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon'		=> array('name' => 'quality', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-				)
-    		);
-    		$nilai = 3.5;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "LITABMAS", 
-    				'link' 		=> '/sdm/profil/Sertifikasi Dosen/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'certificate', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-				)
-    		);
-    		$nilai = 3.6;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Inovasi", 
-    				'link' 		=> '/sdm/profil/Rasio Mahasiswa &#38; Dosen/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'graduate', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-				)
-    		);
-    		$nilai = 1.2;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Keuangan", 
-    				'link' 		=> '/sdm/profil/Dosen Tidak Tetap/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'feedback', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 0
-				)
-    		);
-
-    		$nilai = 1.2;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Sumbar Daya Manusia", 
-    				'link' 		=> '/sdm/profil/Dosen Tidak Tetap/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'feedback', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-				)
-    		);
-
-    		$nilai = 1.2;
-    		array_push($data_profil, 
-    			array(
-    				'title' 	=> "Sumbar Daya Manusia", 
-    				'link' 		=> '/sdm/profil/Dosen Tidak Tetap/'.$nilai,
-    				'skor' 		=> $nilai, 
-					'chart' 	=> array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-					'icon' 		=> array('name' => 'feedback', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 1
-                )
-            );
-            $nilai = 1.2;
-            array_push($data_profil, 
-                array(
-                    'title'     => "Sumbar Daya Manusia", 
-                    'link'      => '/sdm/profil/Dosen Tidak Tetap/'.$nilai,
-                    'skor'      => $nilai, 
-                    'chart'     => array( 'value' => ($nilai*100/4), 'skor'=> $nilai, 'type' => 2 ),
-                    'icon'      => array('name' => 'feedback', 'icon_arr' => array('width' => 50, 'height' => 50) ),
-                    'prog'      => 0
-				)
-    		);
-
-    	return view('home', ['line' => $line, 'data_profil' => $data_profil]);
-    	//return view('home');
-    } 
+        return view('home', [
+            'line' => $nilai,
+            'data_profil' => $nilai_tahun->toArray(),
+        ]);
+    }
 }
