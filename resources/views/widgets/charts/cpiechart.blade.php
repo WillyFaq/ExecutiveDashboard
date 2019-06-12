0d917054ffeed2fbfd4aea16788fbaccec0da205
WillyXevo <willy.xevo@gmail.com>
2019-06-12 20:24:38 +0700

    SDM Tampilan awal saja

--- resources/views/widgets/charts/cpiechart.blade.php
+++ resources/views/widgets/charts/cpiechart.blade.php
@@ -1,2 +1,36 @@
+@php
+	$_idpie = rand(0, 999); 
+@endphp
+<canvas height="155px" id="pie_{{$_idpie}}"></canvas >
+<script>
 
-<canvas class ="round" id="cpie" width="250" height="200"></canvas>
+		var config = {
+			type: 'pie',
+			data: {
+				datasets: [{
+					data: [
+						80,
+						15
+					],
+					backgroundColor: [
+						"#BE1E2D",
+						"#FE8C00"
+					],
+					label: 'Dosen'
+				}],
+				labels: ["Dosen Tetap", "Dosen Tidak Tetap"],
+			},
+			options: {
+				responsive: false,
+    			maintainAspectRatio: false,
+				legend: {
+		            display: false,
+		        }
+			}
+		};
+
+		$(document).ready(function(){
+			var ctxa = document.getElementById('pie_{{$_idpie}}').getContext('2d');
+			window.myPie = new Chart(ctxa, config);
+		});
+	</script>
