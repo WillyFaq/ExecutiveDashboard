7c5c7ff61f1c2da6b3f74383f5e1d2d26c32b028
Rahman Nur Hadi <rahman@stikom.edu>
2019-06-13 18:10:06 +0700

    Cleanup: jalankan `php-cs-fixer` di Model `BerkasPortofolio`

--- app/BerkasPortofolio.php
+++ app/BerkasPortofolio.php
@@ -12,15 +12,15 @@ class BerkasPortofolio extends Model
     public function newQuery()
     {
         return parent::newQuery()
-		->addSelect([
-			'id_berkas',
-			'nik',
-			\DB::Raw('berkas_portofolio_dosen.id_jenis'),
-			'file_path',
-		])
-		->leftJoin('jenis_berkas_portofolio_dosen','berkas_portofolio_dosen.id_jenis','jenis_berkas_portofolio_dosen.id_jenis')
-		->addSelect([
-			'nama_jenis',
-		]);
+        ->addSelect([
+            'id_berkas',
+            'nik',
+            \DB::Raw('berkas_portofolio_dosen.id_jenis'),
+            'file_path',
+        ])
+        ->leftJoin('jenis_berkas_portofolio_dosen', 'berkas_portofolio_dosen.id_jenis', 'jenis_berkas_portofolio_dosen.id_jenis')
+        ->addSelect([
+            'nama_jenis',
+        ]);
     }
 }
