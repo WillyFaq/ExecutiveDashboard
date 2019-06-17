237310d92f1970a586540b6484ad7ec17d261f99
Rahman Nur Hadi <rahman@stikom.edu>
2019-06-17 08:39:46 +0700

    Ubah model `PendaftaranOnline`: cast attribute `tgl_pendaftaran`

--- app/PendaftaranOnline.php
+++ app/PendaftaranOnline.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class PendaftaranOnline extends Model
 {
     protected $table = 'pendaftaran_online';
     protected $primaryKey = 'no_online';
+    protected $casts = [
+        'tgl_daftar' => 'date',
+    ];
 
     public function scopeWhereSudahBayarForm($query)
     {
