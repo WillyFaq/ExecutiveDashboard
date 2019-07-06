c2622a4e26e1539866aba2b0bbef5df60fbb14fa
Rahman Nur Hadi <rahman@stikom.edu>
2019-07-06 14:42:06 +0700

    Fix gauge chart `Beban Kerja Dosen`, `Profil Dosen`, & `Tenaga Kependidikan`

--- resources/views/component_sdm/rasio_mhs_dosen.blade.php
+++ resources/views/component_sdm/rasio_mhs_dosen.blade.php
@@ -10,19 +10,21 @@
     }
 @endphp
 <div class="card mb-2">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-9">
-                <p class="chart-title">{{$judul['180405']}}</p>
-                <p class="data-value text-{{$class_name}}">1 : {{$rasio_dosen_mahasiswa}}</p>
+    <div class="row">
+        <div class="col-8">
+            <div class="card-header flushed pb-0">
+                <p class="chart-title mb-0">{{$judul['180405']}}</p>
             </div>
-            <div class="col-3">
-                @include('widgets.charts.gauge', [
-                    'skor'=> number_format($skor_rasio_dosen_mahasiswa,2), 
-                    'type' => 2 ,
-                    'class_name' => $class_name,
-                ])
+            <div class="card-body">
+                <p class="data-value mb-0 text-{{$class_name}}">1 : {{$rasio_dosen_mahasiswa}}</p>
             </div>
+        </div>
+        <div class="col-4 py-2 mx-auto">
+            @include('widgets.charts.gauge', [
+                'skor'=> number_format($skor_rasio_dosen_mahasiswa,2), 
+                'type' => 2 ,
+                'class_name' => $class_name,
+            ])
         </div>
     </div>
 </div>
