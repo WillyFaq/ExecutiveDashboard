b6ab27f8d3af9702cf3b8ed23658b02805fe817f
WillyXevo <willy.xevo@gmail.com>
2019-07-08 20:31:05 +0700

    update onclick label chart kriteria pt, warna label merah untuk terkecil

--- resources/views/widgets/charts/radarchart.blade.php
+++ resources/views/widgets/charts/radarchart.blade.php
@@ -16,6 +16,20 @@
     @endphp
     var label = {!! json_encode(array_values($label)) !!};
     var data = {!! json_encode(array_values($data)) !!};
+
+    /*   Warna label */
+    var index = 0;
+    var value = data[0];
+    var labelColor = [];
+    for (var i = 1; i < data.length; i++) {
+      	if (data[i] < value) {
+        	value = data[i];
+        	index = i;
+      	}
+      	labelColor.push("#000000");
+    }
+    labelColor[index] = "#FF0000";
+
 		var color = Chart.helpers.color;
 		var config = {
 			type: 'radar',
@@ -44,6 +58,7 @@
 				},
 				scale: {
 					pointLabels: {
+                    	fontColor: labelColor,
 						fontSize: 11,
 						// callback: function(value, index, values) {
 						// 	return value;
@@ -74,9 +89,69 @@
             let chartContext = chartElement.getContext('2d');
             window.myRadar = new Chart(chartContext, config);
             chartElement.onclick = function(e){
+                /*console.log(e);
                 let dot = window.myRadar.getElementAtEvent(e);
                 if(!dot.length) return;
+                console.log(dot);
                 window.location.href = label[dot[0]._index][0].replace(' ','_').toLowerCase();
+				*/
+				var helpers = Chart.helpers;
+
+			    var eventPosition = helpers.getRelativePosition(e, window.myRadar.chart);
+			    var mouseX = eventPosition.x;
+			    var mouseY = eventPosition.y;
+
+			    var activePoints = [];
+			    // loop through all the labels
+			    helpers.each(window.myRadar.scale.ticks, function (label, index) {
+			    	var valueCount = this.pointLabels.length;
+			        for (var i = valueCount - 1; i >= 0; i--) {
+			            // here we effectively get the bounding box for each label
+			            var pointLabelPosition = this.getPointPosition(i, this.getDistanceFromCenterForValue(this.options.reverse ? this.min : this.max) + 5);
+
+			            var pointLabelFontSize = helpers.getValueOrDefault(this.options.pointLabels.fontSize, Chart.defaults.global.defaultFontSize);
+			            var pointLabeFontStyle = helpers.getValueOrDefault(this.options.pointLabels.fontStyle, Chart.defaults.global.defaultFontStyle);
+			            var pointLabeFontFamily = helpers.getValueOrDefault(this.options.pointLabels.fontFamily, Chart.defaults.global.defaultFontFamily);
+			            var pointLabeFont = helpers.fontString(pointLabelFontSize, pointLabeFontStyle, pointLabeFontFamily);
+			            chartContext.font = pointLabeFont;
+
+			            var labelsCount = this.pointLabels.length,
+			                halfLabelsCount = this.pointLabels.length / 2,
+			                quarterLabelsCount = halfLabelsCount / 2,
+			                upperHalf = (i < quarterLabelsCount || i > labelsCount - quarterLabelsCount),
+			                exactQuarter = (i === quarterLabelsCount || i === labelsCount - quarterLabelsCount);
+			            var width = chartContext.measureText(this.pointLabels[i]).width;
+			            var height = pointLabelFontSize;
+
+			            var x, y;
+
+			            if (i === 0 || i === halfLabelsCount)
+			                x = pointLabelPosition.x - width / 2;
+			            else if (i < halfLabelsCount)
+			                x = pointLabelPosition.x;
+			            else
+			                x = pointLabelPosition.x - width;
+
+			            if (exactQuarter)
+			                y = pointLabelPosition.y - height / 2;
+			            else if (upperHalf)
+			                y = pointLabelPosition.y - height;
+			            else
+			                y = pointLabelPosition.y
+
+			            // check if the click was within the bounding box
+			            if ((mouseY >= y && mouseY <= y + height) && (mouseX >= x && mouseX <= x + width))
+			                activePoints.push({ index: i, label: this.pointLabels[i] });
+			        }
+			    }, window.myRadar.scale);
+
+			    var firstPoint = activePoints[0];
+			    if (firstPoint !== undefined) {
+			        //alert(firstPoint.index + ': ' + firstPoint.label);
+			    	var lab = firstPoint.label;
+			    	//console.log(lab[0].toLowerCase());
+			    	window.location.href=lab[0].toLowerCase();
+			    }
 			}
 		});
 
